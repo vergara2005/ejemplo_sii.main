c61204ef40a55affb5f4fb3f696383ef19500b16
Claudio Vergara <claudio.vergara@sii.cl>
2022-05-24 18:59:35 -0400

    Agregar Frances

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const port = process.env.PORT || 3040;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hola Feature cambio idioma de sii!\n'
-  const fr = 'Bounjour!\n'
-  const eng = 'Hello world!\n'
+  const fr = 'Hola Feature cambio idioma de sii!\n'
   res.end(msg + " " + fr + " " + eng);
 });
 
